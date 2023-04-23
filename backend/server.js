f536ec61990628a13e22c4927ac79182a66fdfa6
am-here <harshbaid.ind@gmail.com>
2023-04-23 21:46:43 +0530

    deployed 1.0.1 (backend)

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://communichat.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://communichat.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
